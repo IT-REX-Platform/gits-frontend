f1819f39e94edfd7c20390a71c978506ffd6ce22
Valentin Morlock <valentin@morlock.io>
2023-04-25 11:19:32 +0000

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,30 @@
+import { RelayEnvironmentProvider } from "react-relay";
+import { initRelayEnvironment } from "../src/RelayEnvironment";
+import { RecordSource } from "relay-runtime";
+import { useMemo, useEffect } from "react";
+import { RelayPageProps } from "../src/relay-types";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<RelayPageProps>) {
+  const environment = useMemo(initRelayEnvironment, []);
+
+  useEffect(() => {
+    const store = environment.getStore();
+
+    // Hydrate the store.
+    store.publish(new RecordSource(pageProps.initialRecords));
+
+    // Notify any existing subscribers.
+    store.notify();
+  }, [environment, pageProps.initialRecords]);
+
+  return (
+    <RelayEnvironmentProvider environment={environment}>
+      <Component {...pageProps} />
+    </RelayEnvironmentProvider>
+  );
+}

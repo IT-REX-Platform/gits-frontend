f0d11e525f67229f2564cc02066df42d4476deeb
Robert <st148766@stud.uni-stuttgart.de>
2023-05-19 20:04:58 +0200

    fixed wrong proportions of the UI

--- pages/course/[id]/details.tsx
+++ pages/course/[id]/details.tsx
@@ -39,16 +39,15 @@ export default function Details() {
   );
   const course = courses.find((course) => course.id === id)!;
   return (
-    <div className="flex">
-      <div className="grid grid-flow-dense grid-cols-6 grid-rows-6 gap-2">
-        <div className="col-span-full row-span-1">{course.title}</div>
-        <div className="col-span-full row-span-1">{course.description}</div>
-        <div className="col-span-3 row-span-2 bg-orange-600">Chapters</div>
-        <div className="col-span-3 row-span-2 bg-yellow-400">Skill levels</div>
-        <div className="col-span-2 row-span-2 bg-blue-500">FlashCards</div>
-        <div className="col-span-2 row-span-2 bg-green-500">Quizzes</div>
-        <div className="col-span-2 row-span-2 bg-rose-400">Assignments</div>
-      </div>
+    
+    <div className="grid grid-flow-dense grid-cols-6 grid-rows-6 gap-2">
+      <div className="col-span-full row-span-1">{course.title}</div>
+      <div className="col-span-full row-span-1">{course.description}</div>
+      <div className="col-span-3 row-span-2 bg-orange-600">Chapters</div>
+      <div className="col-span-3 row-span-2 bg-yellow-400">Skill levels</div>
+      <div className="col-span-2 row-span-2 bg-blue-500">FlashCards</div>
+      <div className="col-span-2 row-span-2 bg-green-500">Quizzes</div>
+      <div className="col-span-2 row-span-2 bg-rose-400">Assignments</div>
     </div>
   );
 }

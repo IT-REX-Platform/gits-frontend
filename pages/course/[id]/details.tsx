bfaed2ab252e840ea788584046f458e5f2a0f9fd
Robert <st148766@stud.uni-stuttgart.de>
2023-05-21 14:51:50 +0200

    added mock info for demonstration

--- pages/course/[id]/details.tsx
+++ pages/course/[id]/details.tsx
@@ -7,6 +7,8 @@ import {
   ReactFragment,
   ReactPortal,
 } from "react";
+import { VictoryLabel, VictoryPie } from "victory";
+import Link from "next/link";
 
 export default function Details() {
   const router = useRouter();
@@ -24,7 +26,7 @@ export default function Details() {
     `,
     {id}
   ); */
-
+  //placeholder query until coursesById is correctly implemented
   const { courses } = useLazyLoadQuery<detailsQuery>(
     graphql`
       query detailsQuery {
@@ -38,16 +40,168 @@ export default function Details() {
     {}
   );
   const course = courses.find((course) => course.id === id)!;
+
+  //TODO: change later, when implementing the services into this side
+  const chapters = [
+    "Chapter 1",
+    "Chapter 2",
+    "Chapter 3",
+    "Chapter 4",
+    "Chapter 5",
+    "Chapter 6",
+    "Chapter 7",
+    "Chapter 8",
+    "Chapter 9",
+    "Chapter 10",
+  ];
+  const knowledge = 88;
+  const understanding = 50;
+  const analyses = 40;
+  const usage = 22;
+  const flashcards = [
+    "Cards 1",
+    "Cards 2",
+    "Cards 3",
+    "Cards 4",
+    "Cards 5",
+    "Cards 6",
+    "Cards 7",
+    "Cards 8",
+    "Cards 9",
+    "Cards 10",
+  ];
+  const quizzes = [
+    "Quiz 1",
+    "Quiz 2",
+    "Quiz 3",
+    "Quiz 4",
+    "Quiz 5",
+    "Quiz 6",
+    "Quiz 7",
+    "Quiz 8",
+    "Quiz 9",
+    "Quiz 10",
+  ];
+  const assignemnts = [
+    "Assignment 1",
+    "Assignment 2",
+    "Assignment 3",
+    "Assignment 4",
+  ];
+
   return (
-    
     <div className="grid grid-flow-dense grid-cols-6 grid-rows-6 gap-2">
-      <div className="col-span-full row-span-1">{course.title}</div>
-      <div className="col-span-full row-span-1">{course.description}</div>
-      <div className="col-span-3 row-span-2 bg-orange-600">Chapters</div>
-      <div className="col-span-3 row-span-2 bg-yellow-400">Skill levels</div>
-      <div className="col-span-2 row-span-2 bg-blue-500">FlashCards</div>
-      <div className="col-span-2 row-span-2 bg-green-500">Quizzes</div>
-      <div className="col-span-2 row-span-2 bg-rose-400">Assignments</div>
+      <div className="col-span-full row-span-1 m-5 font-bold text-2xl underline">
+        {course.title}
+      </div>
+      <div className="col-span-full row-span-1 m-5 text-xl">
+        {course.description}
+      </div>
+      <div className="col-span-3 row-span-2 border-solid border-sky-900 border-2 m-5 rounded-lg ">
+        <p className="underline">Chapters</p>
+        <div className="flex flex-col gap-1">
+          {chapters.map((chapter) => (
+            <Link
+              className="font-bold text-white text-center bg-sky-900 hover:bg-transparent hover:text-sky-900 hover:border-solid hover:border-sky-900 hover:border-2 rounded-lg"
+              href={`/`}
+            >
+              <div>{chapter}</div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="col-span-3 row-span-2 border-solid border-sky-900 border-2 m-5 rounded-lg ">
+        <p className="underline">Skill levels</p>
+        <div className="grid grid-cols-2 grid-rows-2">
+          <VictoryPie
+            colorScale={["green", "transparent"]}
+            innerRadius={120}
+            cornerRadius={100}
+            labels={["Wissen"]}
+            labelRadius={1}
+            labelPosition={"startAngle"}
+            data={[{ y: knowledge }, { y: 100 - knowledge }]}
+            width={1250}
+            style={{ labels: { fontSize: 50, fill: "black" } }}
+            labelComponent={<VictoryLabel dy={5} />}
+          />
+          <VictoryPie
+            colorScale={["yellow", "transparent"]}
+            innerRadius={120}
+            cornerRadius={100}
+            labels={["Verstand"]}
+            labelRadius={1}
+            labelPosition={"startAngle"}
+            data={[{ y: understanding }, { y: 100 - understanding }]}
+            width={1250}
+            style={{ labels: { fontSize: 50, fill: "black" } }}
+            labelComponent={<VictoryLabel dy={5} />}
+          />
+          <VictoryPie
+            colorScale={["red", "transparent"]}
+            innerRadius={120}
+            cornerRadius={100}
+            labels={["Analyse"]}
+            labelRadius={1}
+            labelPosition={"startAngle"}
+            data={[{ y: analyses }, { y: 100 - analyses }]}
+            width={1250}
+            style={{ labels: { fontSize: 50, fill: "black" } }}
+            labelComponent={<VictoryLabel dy={5} />}
+          />
+          <VictoryPie
+            colorScale={["red", "transparent"]}
+            innerRadius={120}
+            cornerRadius={100}
+            labels={["Anwend."]}
+            labelRadius={1}
+            labelPosition={"startAngle"}
+            data={[{ y: usage }, { y: 100 - usage }]}
+            width={1250}
+            style={{ labels: { fontSize: 50, fill: "black" } }}
+            labelComponent={<VictoryLabel dy={5} />}
+          />
+        </div>
+      </div>
+      <div className="col-span-2 row-span-2 border-solid border-sky-900 border-2 m-5 rounded-lg ">
+        <p className="underline">Flashcards</p>
+        <div className="flex flex-col gap-1">
+          {flashcards.map((flashcard) => (
+            <Link
+              className="font-bold text-white text-center bg-sky-900 hover:bg-transparent hover:text-sky-900 hover:border-solid hover:border-sky-900 hover:border-2 rounded-lg"
+              href={`/`}
+            >
+              <div>{flashcard}</div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="col-span-2 row-span-2 border-solid border-sky-900 border-2 m-5 rounded-lg ">
+        <p className="underline">Quizzes</p>
+        <div className="flex flex-col gap-1">
+          {quizzes.map((quiz) => (
+            <Link
+              className="font-bold text-white text-center bg-sky-900 hover:bg-transparent hover:text-sky-900 hover:border-solid hover:border-sky-900 hover:border-2 rounded-lg"
+              href={`/`}
+            >
+              <div>{quiz}</div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="col-span-2 row-span-2 border-solid border-sky-900 border-2 m-5 rounded-lg ">
+        <p className="underline">Assignments</p>
+        <div className="flex flex-col gap-1">
+          {assignemnts.map((assignemnt) => (
+            <Link
+              className="font-bold text-white text-center bg-sky-900 hover:bg-transparent hover:text-sky-900 hover:border-solid hover:border-sky-900 hover:border-2 rounded-lg"
+              href={`/`}
+            >
+              <div>{assignemnt}</div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }

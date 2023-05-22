bbb2387ea5578b5d76c2663199df55b67842fde6
Marcel Marquardt <marcel@mardom.dev>
2023-05-22 12:26:48 +0200

    Remove required from publish switch

--- pages/course/create.tsx
+++ pages/course/create.tsx
@@ -121,7 +121,6 @@ export default function NewCourse() {
           </table>
           <FormControlLabel
             label="Publish course"
-            required
             control={
               <Switch
                 checked={publish}

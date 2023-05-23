bc6518ebd02f5f78b14f537c1fb866dd0dee09dc
Robert Philippsohn <32335844+SkyDiverStar@users.noreply.github.com>
2023-05-23 08:52:03 +0200

    Update pages/course/[id]/details.tsx
    Suggested commit 2, have to test later before further changes
    
    Co-authored-by: Valentin Morlock <valentin@morlock.io>

--- pages/course/[id]/details.tsx
+++ pages/course/[id]/details.tsx
@@ -14,7 +14,7 @@ export default function Details() {
   const router = useRouter();
   const id = router.query.id;
 
-  /* const { coursesById } = useLazyLoadQuery<detailsQuery>(
+  const { coursesById } = useLazyLoadQuery<detailsQuery>(
     graphql`
       query detailsQuery($id: [UUID!]!) {
         coursesById(ids: $id) {
@@ -23,22 +23,9 @@ export default function Details() {
         }
       }
     `,
-    {id}
-  ); */
-  //placeholder query until coursesById is correctly implemented
-  const { courses } = useLazyLoadQuery<detailsQuery>(
-    graphql`
-      query detailsQuery {
-        courses {
-          id
-          title
-          description
-        }
-      }
-    `,
-    {}
+    { id: [id] }
   );
-  const course = courses.find((course) => course.id === id)!;
+  const course = coursesById[0];
 
   //TODO: change later, when implementing the services into this side
   const chapters = [

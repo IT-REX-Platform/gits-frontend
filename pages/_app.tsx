061827307d6f9927f3fc2756eb1db7a0cc7d4599
Valentin Morlock <valentin@morlock.io>
2023-06-08 21:18:45 +0000

    add token to graphql requests

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,32 +31,33 @@ const oidcConfig: AuthProviderProps = {
 };
 
 export default function App(props: AppProps<RelayPageProps>) {
-  const environment = useMemo(() => initRelayEnvironment(), []);
-
-  // re-hydrate the relay store (requires special getServerSide... https://github.com/vercel/next.js/blob/canary/examples/with-relay-modern/pages/index.js)
-  useEffect(() => {
-    const store = environment.getStore();
-    // Hydrate the store.
-    store.publish(new RecordSource(props.pageProps.initialRecords));
-    // Notify any existing subscribers.
-    store.notify();
-  }, [environment, props.pageProps.initialRecords]);
-
   return (
     <AuthProvider {...oidcConfig}>
-      <RelayEnvironmentProvider environment={environment}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <Suspense fallback="Loading...">
-            <SigninContent {...props} />
-          </Suspense>
-        </LocalizationProvider>
-      </RelayEnvironmentProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Suspense fallback="Loading...">
+          <SigninContent {...props} />
+        </Suspense>
+      </LocalizationProvider>
     </AuthProvider>
   );
 }
 
 function SigninContent({ pageProps, Component }: AppProps<RelayPageProps>) {
   const auth = useAuth();
+
+  const environment = useMemo(
+    () => initRelayEnvironment(auth.user?.access_token),
+    [auth.user?.access_token]
+  );
+
+  // re-hydrate the relay store (requires special getServerSide... https://github.com/vercel/next.js/blob/canary/examples/with-relay-modern/pages/index.js)
+  useEffect(() => {
+    const store = environment.getStore();
+    // Hydrate the store.
+    store.publish(new RecordSource(pageProps.initialRecords));
+    // Notify any existing subscribers.
+    store.notify();
+  }, [environment, pageProps.initialRecords]);
 
   // automatically sign-in
   useEffect(() => {
@@ -92,12 +93,14 @@ function SigninContent({ pageProps, Component }: AppProps<RelayPageProps>) {
 
   if (auth.isAuthenticated) {
     return (
-      <div className="grid grid-cols-[auto_1fr] h-[100vh] overflow-hidden">
-        <Navbar />
-        <div className="overflow-auto">
-          <Component {...pageProps} />
+      <RelayEnvironmentProvider environment={environment}>
+        <div className="grid grid-cols-[auto_1fr] h-[100vh] overflow-hidden">
+          <Navbar />
+          <div className="overflow-auto">
+            <Component {...pageProps} />
+          </div>
         </div>
-      </div>
+      </RelayEnvironmentProvider>
     );
   }
 

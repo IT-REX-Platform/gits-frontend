eb31c2a5699aef9d75fb7bff20c4094f21e925f9
Robert <st148766@stud.uni-stuttgart.de>
2023-07-13 22:56:25 +0200

    styling of scoreboard site

--- app/courses/[courseId]/scoreboard/page.tsx
+++ app/courses/[courseId]/scoreboard/page.tsx
@@ -10,73 +10,51 @@ import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 
 interface Column {
-  id: "name" | "code" | "population" | "size" | "density";
+  id: "place" | "name" | "power";
   label: string;
   minWidth?: number;
-  align?: "right";
+  align?: "right" | "center";
   format?: (value: number) => string;
 }
 
 const columns: readonly Column[] = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+  { id: "place", label: "Place", minWidth: 100 },
+  { id: "name", label: "Name", minWidth: 200, align: "center" },
   {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
+    id: "power",
+    label: "Power",
+    minWidth: 100,
     align: "right",
     format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toFixed(2),
   },
 ];
 
 interface Data {
+  place: string;
   name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
+  power: number;
 }
 
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number
-): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData(place: string, name: string, power: number): Data {
+  return { place, name, power };
 }
 
 const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
+  createData("India", "IN", 1324171354),
+  createData("China", "CN", 1403500365),
+  createData("Italy", "IT", 60483973),
+  createData("United States", "US", 327167434),
+  createData("Canada", "CA", 37602103),
+  createData("Australia", "AU", 25475400),
+  createData("Germany", "DE", 83019200),
+  createData("Ireland", "IE", 4857000),
+  createData("Mexico", "MX", 126577691),
+  createData("Japan", "JP", 126317000),
+  createData("France", "FR", 67022000),
+  createData("United Kingdom", "GB", 67545757),
+  createData("Russia", "RU", 146793744),
+  createData("Nigeria", "NG", 200962417),
+  createData("Brazil", "BR", 210147125),
 ];
 
 export default function StickyHeadTable() {
@@ -96,7 +74,7 @@ export default function StickyHeadTable() {
 
   return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+      <TableContainer sx={{ maxHeight: "100%" }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
@@ -105,6 +83,8 @@ export default function StickyHeadTable() {
                   key={column.id}
                   align={column.align}
                   style={{ minWidth: column.minWidth }}
+                  sx={{ backgroundColor: "#90a4ae" }}
+                  className="border-solid border-2"
                 >
                   {column.label}
                 </TableCell>
@@ -116,11 +96,15 @@ export default function StickyHeadTable() {
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.name}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
-                        <TableCell key={column.id} align={column.align}>
+                        <TableCell
+                          key={column.id}
+                          align={column.align}
+                          className="border-solid border-2"
+                        >
                           {column.format && typeof value === "number"
                             ? column.format(value)
                             : value}

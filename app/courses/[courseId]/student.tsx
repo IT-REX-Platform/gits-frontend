59850232aa6c97850c46270d7ea7c23949961e68
Robert <st148766@stud.uni-stuttgart.de>
2023-08-31 17:41:12 +0200

    fix-null-users

--- app/courses/[courseId]/student.tsx
+++ app/courses/[courseId]/student.tsx
@@ -134,7 +134,7 @@ export default function StudentCoursePage() {
   // Extract scoreboard
   const rows: Data[] = scoreboard
     .slice(0, 3)
-    .map((element) => createData(element.user.userName, element.powerScore));
+    .map((element) => createData(element.user!.userName, element.powerScore));
 
   // Extract course
   const course = coursesByIds[0];

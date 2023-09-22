082684e31227a5b5ba83b150a0b8db32bd301c52
Marcel Marquardt <marcel@mardom.dev>
2023-09-22 14:10:08 +0000

    Replace error pages with global one

--- app/courses/[courseId]/media/[mediaId]/error.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { PageError } from "@/components/PageError";
-import { cleanupErrorMessage } from "@/src/utils";
-
-export default function MediaError({ error }: { error: Error }) {
-  return <PageError message={cleanupErrorMessage(error.message)} />;
-}

bdf9445786e63a972b9e60f6a9e1122b4fb06ac9
Valentin Morlock <valentin@morlock.io>
2023-10-12 20:41:01 +0200

    Fix adding stage

--- components/EditContentModal.tsx
+++ components/EditContentModal.tsx
@@ -59,6 +59,7 @@ export function EditContentModal({
       fragment EditContentModalFragment on Chapter {
         ...AddFlashcardSetModalFragment
         sections {
+          id
           stages {
             id
             optionalContents {
